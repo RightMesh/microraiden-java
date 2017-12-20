de386cd3cfd9165a84eb465158be2b12a86641e7
Jason Ernst <jason@left.io>
2017-12-20 12:34:42 -0800

    Added ability to create and list ethereum accounts

--- src/main/java/MicroRaiden.java
+++ src/main/java/MicroRaiden.java
@@ -1,5 +1,13 @@
 import java.lang.reflect.*;
 import java.util.Arrays;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.io.OutputStream;
+import java.io.InputStream;
+import java.io.IOException;
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.io.IOUtils;
 import org.ethereum.crypto.ECKey;
 
 /*
@@ -19,6 +27,64 @@ public class MicroRaiden {
         //to store the files
     }
     
+    /**
+     * Create a new ethereum account to be used for testing microraiden
+     * channels. Stores the account in the same folder where the
+     * program is run. Note - there is no encryption on this private key
+     * so it should be used for anything real!!
+     * @param accountFile - the name of the output file for the account
+     */
+    public void createAccount(String accountFile) {
+        ECKey keyPair = new ECKey();
+        String address = new String(Hex.encodeHex(keyPair.getAddress()));
+        System.out.println("Generated new account: 0x" + address);
+        byte[] priv = keyPair.getPrivKeyBytes();
+        byte[] nodeid = keyPair.getNodeId();
+        
+        try {
+            OutputStream os = new FileOutputStream(accountFile + ".pkey");
+            os.write(priv);
+            os.close();
+        } catch (IOException e) {
+            System.out.println("Couldn't write to file: " + accountFile + " " + e.toString());
+        }
+    }
+    
+    /**
+     * Reads all of the account files in the directory and lists which
+     * are usable for testing channels with. Looks for files with the
+     * .pkey extension.
+     */
+    public void listAccounts() {
+        File dir = new File(".");
+        File[] filesList = dir.listFiles();
+        for (File file : filesList) {
+            if (file.isFile()) {
+                if(file.getName().contains(".pkey")) {
+                    //System.out.println(file.getName());
+                    try {
+                        InputStream is = new FileInputStream(file.getName());
+                        byte[] priv = IOUtils.toByteArray(is);
+                        is.close();
+                        ECKey keyPair = ECKey.fromPrivate(priv);
+                        String address = new String(Hex.encodeHex(keyPair.getAddress()));
+                        System.out.println("0x" + address);
+                    } catch(IOException ex) {
+                        System.out.println("Couldn't read from file: " + file.getName() + " " + ex.toString());
+                    }
+                }
+            }
+        }
+    }
+    
+    /**
+     * Lists the balances for a particular account and any balance
+     * in the channel with the given remote peer
+     */
+    public void listBalances(String account, String remotePeer) {
+        
+    }
+    
     public void createChannel() {
         System.out.println("CREATE CHANNEL");
     }
@@ -31,23 +97,85 @@ public class MicroRaiden {
         System.out.println("CLOSE CHANNEL");
     }
     
-    
+    /**
+     * Displays a list of all available functions
+     */
+    private static void displayFunctions() {
+        System.out.println("Possible Functions: ");
+        Class cls = MicroRaiden.class;
+        Method methlist[] = cls.getDeclaredMethods();
+        for (int i = 0; i < methlist.length; i++) {
+            Method m = methlist[i];
+            if(m.getName().equals("main") || m.getName().equals("displayFunctions")) {
+                continue;
+            }
+            
+            String params = "";
+            Class pvec[] = m.getParameterTypes();
+            for (int j = 0; j < pvec.length; j++) {
+                params = params + pvec[j];
+                if(j < (pvec.length - 1)) {
+                    params = params + ", ";
+                }
+            }
+            
+            System.out.println("  " + m.getReturnType() + " " + m.getName() + "(" + params + ")");
+        }
+    }
     
     public static void main(String[] args) throws Exception {
-        MicroRaiden m = new MicroRaiden();
+        MicroRaiden mr = new MicroRaiden();
         
         if(args.length < 1) {
             System.out.println("Usage: microraiden-java <function> <args>");
+            displayFunctions();
             return;
         }
         
         //get the function name - use reflection to call
         String functionName = args[0];
         
+        //some trickery to get the method with the params (if we just
+        //try to search for the method without the params specified it
+        //will only look for parameter-less version
         Class cls = Class.forName("MicroRaiden");
-        Method meth = cls.getMethod(functionName);
+        Method method = null; 
+        Method methlist[] = cls.getDeclaredMethods();
+        for (int i = 0; i < methlist.length; i++) {
+            Method m = methlist[i];
+            if(m.getName().equals(functionName)) {
+                method = m;
+                break;
+            }
+        }
         
-        Object arglist[] = Arrays.copyOfRange(args, 1, args.length);
-        Object retobj = meth.invoke(m, arglist);
+        //cast the args to the correct type for the function params
+        //note if you use a weird type this will probably shit itself.
+        Object arglist[] = new Object[args.length - 1];
+        Class pvec[] = method.getParameterTypes();
+        for(int i = 1; i < args.length; i++) {
+            if(pvec.length < i) {
+                break;
+            }
+            
+            String argtype = args[i].getClass().getName();
+            String actualType = pvec[i-1].getName();
+            if(!argtype.equals(actualType)) {
+                switch(actualType) {
+                    case "int": {
+                        arglist[i-1] = Integer.parseInt(args[i]);
+                        break;
+                    }
+                    default: {
+                        System.out.println("UNKNOWN PARAM TYPE: " + actualType);
+                        return;
+                    }
+                }
+            } else {
+                arglist[i-1] = args[i];
+            }
+        }
+        
+        Object retobj = method.invoke(mr, arglist);
     }
 }
